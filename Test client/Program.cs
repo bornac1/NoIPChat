739801093adc69ae8084bf2abd1a4113968f8258
Borna <Borna>
2023-12-03 21:59:54 +0100

    Test client

--- Test client/Program.cs
+++ Test client/Program.cs
@@ -1,33 +1,74 @@
 ﻿using Messages;
 using System;
+using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Test_client
 {
-    internal class Program
+    public class Client
     {
-        public bool auth = false;
-        private static void Print(byte[] bytes)
+        public float CV = 1;
+        public bool? auth;
+        public bool connected = false;
+        public ConcurrentQueue<Messages.Message> messages;
+        public string? Username;
+        public string? Password;
+        public string? Server;
+        public TcpClient client;
+        public NetworkStream? stream;
+        private readonly Processing processing;
+        private readonly byte[] buffer = new byte[1024];
+        private int bytesRead;
+        private int bufferOffset;
+        public ConcurrentQueue<Messages.Message> messages_rec;
+        private readonly StringBuilder value;
+        public bool ischatready = false;
+        private bool disconnectstarted;
+        public Messages.Message message;
+        public Client()
         {
-            foreach (byte b in bytes)
+            disconnectstarted = false;
+            processing = new Processing();
+            messages = [];
+            messages_rec = [];
+            value = new StringBuilder();
+            client = new TcpClient();
+            message = new Messages.Message();
+        }
+        public async Task Connect()
+        {
+            try
             {
-                string byteString = b.ToString("X2"); // Convert to hexadecimal string
-                Console.Write(byteString + " ");
+                client = new TcpClient();
+                await client.ConnectAsync(IPAddress.Parse("192.168.114.169"), 10001);
+                stream = client.GetStream();
+                connected = true;
+            }
+            catch (Exception ex)
+            {
+                if (ex is SocketException)
+                {
+                    //Error connecting
+                    await Disconnect();
+                }
+                else
+                {
+                    //Clean all
+                    await Disconnect();
+                    //Logging
+                }
             }
         }
-        private async Task Receive(NetworkStream stream)
+        private async Task Receive()
         {
-            int bytesRead = 0;
-            int bufferOffset = 0;
-            byte[] buffer= new byte[1024];
-            Processing processing = new Processing();
-            while (true)
+            while (connected)
             {
                 try
                 {
@@ -55,99 +96,179 @@ namespace Test_client
                             //Message processing starts
                             try
                             {
-                                Message message = await processing.Deserialize(messageBytes);
-                                if (message.Auth == true)
-                                {
-                                    auth = true;
-                                }
-                                Console.WriteLine(message.Sender + ":" + message.Msg);
+                                Messages.Message message = await processing.Deserialize(messageBytes);
+                                await ProcessMessage(message);
                             }
                             catch (Exception ex)
                             {
-                                Console.WriteLine("Message deseialization error");
-                                Console.WriteLine(ex.ToString());
+                                //Can't be fixed
+                                //There is error with the message
+                                //Just give up
                             }
                         }
                     }
 
                     // Read more bytes from the stream
-                    int bytesReadNow = await stream.ReadAsync(buffer, bytesRead, buffer.Length - bytesRead);
-
-                    // Check if the stream has reached its end
-                    if (bytesReadNow == 0)
+                    if (stream != null)
                     {
-                        break;
-                    }
+                        int bytesReadNow = await stream.ReadAsync(buffer, bytesRead, buffer.Length - bytesRead);
 
-                    bytesRead += bytesReadNow;
+                        // Check if the stream has reached its end
+                        if (bytesReadNow == 0)
+                        {
+                            connected = false;
+                            break;
+                        }
+
+                        bytesRead += bytesReadNow;
+                    }
                 }
                 catch (Exception ex)
-                { //assume disconnection
+                {
+                    if (ex is IOException)
+                    {
+                        //assume disconnection
+                        await Disconnect();
+                    }
+                    else
+                    {
+                        //Logging
+                        //Clean all
+                        await Disconnect();
+                    }
                 }
             }
         }
-        static void Main(string[] args)
+        public async Task Login(string username, string password)
         {
-            string username;
-            TcpClient client= new TcpClient();
-            Console.Write("Ip:");
-            string ip = Console.ReadLine();
-            Console.Write("Port:");
-            string port = Console.ReadLine();
-            client.Connect(IPAddress.Parse(ip), int.Parse(port));
-            NetworkStream stream = client.GetStream();
-            Processing processing = new Processing();
-            Message message = new Message();
-            message.CV = 1;
-            Console.Write("Username:");
-            username =  Console.ReadLine();
-            message.User = username;
-            Console.Write("Password:");
-            message.Pass = Console.ReadLine();
-            byte[] data = processing.Serialize(message).Result;
-            stream.Write(BitConverter.GetBytes(data.Length));
-            stream.Write(data);
-            Program program = new Program();
-            _ = program.Receive(stream);
-            int c = 0;
-            while (!program.auth)
+            Username = username.ToLower();
+            Password = password;
+            message.CV = CV;
+            message.User = Username;
+            message.Pass = Password;
+            if (await SendMessage(message))
             {
-                Task.Delay(10);
-                Console.WriteLine("Auth:"+c + program.auth);
-                c++;
+                _ = Receive();
             }
-            Console.WriteLine("Authenticated");
-            string Msg = "";
-            while (Msg != "kraj")
+        }
+        public async Task<bool> SendMessage(Messages.Message message)
+        {
+            bool msgerror = false;
+            try
             {
-                message = new Message();
-                message.CV = 1;
-                Console.Write("Reciver:");
-                message.Receiver = Console.ReadLine();
-                message.Sender = username;
-                Msg = Console.ReadLine();
-                message.Msg = Msg;
-                if (Msg != "kraj") {
-                    data = processing.Serialize(message).Result;
-                    stream.Write(BitConverter.GetBytes(data.Length));
-                    stream.Write(data);
+                byte[]? data = await processing.Serialize(message);
+                if (data != null)
+                {
+                    byte[] length = BitConverter.GetBytes(data.Length);
+                    if (stream != null)
+                    {
+                        await stream.WriteAsync(length);
+                        await stream.WriteAsync(data);
+                        return true;
+                    }
+                    return false;
                 }
-                else{
-                    break;
+                else
+                {
+                    //Message error
+                    msgerror = true;
                 }
             }
-            message = new Message();
-            message.CV = 1;
-            message.Disconnect = true;
-            data = processing.Serialize(message).Result;
-            stream.Write(BitConverter.GetBytes(data.Length));
-            stream.Write(data);
-            stream.Flush();
-            stream.Close();
-            stream.Dispose();
-            client.Close();
-            client.Dispose();
-            Console.WriteLine("Disconnected");
+            catch (Exception ex)
+            {
+                if (ex is IOException)
+                {
+                    //Assume disconnection
+                    await Disconnect();
+                    //Save message to be sent later
+                    if (!msgerror)
+                    {
+                        messages.Enqueue(message);
+                    }
+                    else
+                    {
+                        //Message error
+                        //Give up
+                    }
+                }
+                else
+                {
+                    //Logging
+                }
+            }
+            return false;
+        }
+        private async Task ProcessMessage(Messages.Message message)
+        {
+            if (message.Auth == true)
+            {
+                //User is authenticated
+                auth = true;
+            }
+            else if (message.Auth == false)
+            {
+                auth = false;
+            }
+            else if (message.Msg != null || message.Data != null)
+            {
+                await PrintMessage(message);
+            }
+        }
+        public async Task PrintMessage(Messages.Message message)
+        {
+            Console.WriteLine(message.Msg);
+        }
+        public async Task Disconnect(bool force = false)
+        {
+            if (!disconnectstarted)
+            {
+                disconnectstarted = true;
+                try
+                {
+                    connected = false;
+                    if (stream != null)
+                    {
+                        await stream.FlushAsync();
+                        stream.Close();
+                        await stream.DisposeAsync();
+                    }
+                    if (client != null)
+                    {
+                        client.Close();
+                        client.Dispose();
+                    }
+                    await processing.Close();
+                }
+                catch (Exception ex)
+                {
+                    //Logging
+                }
+            }
+        }
+        public static void Main()
+        {
+            for (int i = 0; i<100; i++)
+            {
+                Thread T = new Thread(new ThreadStart(async () => {
+                    var c = new Client();
+                    await c.Connect();
+                    await c.Login("client" + i + "@server1", "pass");
+                    while (c.auth != true)
+                    {
+                        await Task.Delay(10);
+                    }
+                    for (int j = 0; j < 100; j++)
+                    {
+                        await c.SendMessage(new Message()
+                        {
+                            Receiver = "client"+i+"@server1",
+                            Sender = "client" + i + "@server1",
+                            Msg = "Test message "+j+" from client "+i
+                        });
+                        Thread.Sleep(1000);
+                    }
+                })); T.Start(); T.Join(); Thread.Sleep(10);
+            }
             Console.ReadLine();
         }
     }
