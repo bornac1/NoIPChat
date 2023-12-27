ebfc42244dc68b5ec524ffba60e302809589883c
Borna <Borna>
2023-12-27 19:21:32 +0100

    Work on shell console started

---
+++ Server/Shell.cs
@@ -0,0 +1,44 @@
+﻿namespace Server
+{
+    internal class Shell
+    {
+        //Server command line interface
+        private Server server;
+        //Commands strings
+        private const string exit = "exit";
+        public Shell(Server server) {
+            this.server = server;
+            Loop();
+        }
+        private void Loop () {
+            while (true)
+            {
+                ProcessCommand(Console.ReadLine());
+            }
+        }
+        private void ProcessCommand(string? command)
+        {
+            if (command != null)
+            {
+                ReadOnlySpan<char> comm = command.AsSpan();
+                int i = 0;
+                while (i < comm.Length)
+                {
+                    //skip whitespaces
+                    while (comm[i] == ' ' && i < comm.Length)
+                    {
+                        i += 1;
+                    }
+                }
+                ExecuteCommand(comm);
+            }
+        }
+        private void ExecuteCommand (ReadOnlySpan<char> command)
+        {
+            if(MemoryExtensions.Equals(command, exit, StringComparison.OrdinalIgnoreCase))
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
