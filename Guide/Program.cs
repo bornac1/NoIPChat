16f9fecdd71529fdcbbed805c2835aa1b2d19a7e
bornac1 <borna.cesarec1@gmail.com>
2024-12-18 08:01:05 +0100

    Added support for client configuration

--- Guide/Program.cs
+++ Guide/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Serialization;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Xml.Serialization;
 using ConfigurationData;
 namespace Guide
 {
@@ -6,8 +8,35 @@ namespace Guide
     {
         static void Client()
         {
-            Console.WriteLine("Client currently isn't supported.");
-            Begin();
+            Console.WriteLine("What do you need to do with Client?");
+            Console.Write("Type I for installation, C for Configuration or B to go back to main menu.");
+        clientstart:
+            string? s = Console.ReadLine();
+            if (s != null)
+            {
+                if (s.Equals("I", StringComparison.OrdinalIgnoreCase))
+                {
+                    //TODO: installation
+                    Console.WriteLine("Installation currently isn't supported.");
+                    Begin();
+
+                }
+                else if (s.Equals("C", StringComparison.OrdinalIgnoreCase))
+                {
+                    ClientConfig();
+                    Begin();
+
+                }
+                else if (s.Equals("B", StringComparison.OrdinalIgnoreCase))
+                {
+                    Begin();
+                }
+                else
+                {
+                    Console.WriteLine("Unknown. Try again.");
+                    goto clientstart;
+                }
+            }
         }
         static void Server()
         {
@@ -146,6 +175,87 @@ namespace Guide
                     Console.WriteLine("Unknown. Try again.");
                     goto serverconfigstart;
                 }
+            }
+        }
+        static void ClientConfig()
+        {
+        clientconfigstart:
+            Console.WriteLine("Welcome to Client configurator for NoIPChat.");
+            string path = Directory.GetCurrentDirectory();
+        checkpath:
+            Console.WriteLine("Path: " + path);
+            Console.Write("Is the path above pointing to the NoIPChat Client folder? Type Y for yes or N for no. ");
+        input:
+            string? c = Console.ReadLine();
+            if (c != null)
+            {
+                if (c.Equals("Y", StringComparison.OrdinalIgnoreCase))
+                {
+                    //Correct path, continue
+                    if (File.Exists(Path.Combine(path, "Servers.json")))
+                    {
+                        Console.WriteLine("Servers list already exists.");
+                        Console.WriteLine("Old servers list will be renamed to Servers.json.backup.");
+                        File.Move(Path.Combine(path, "Servers.json"), Path.Combine(path, "Servers.json.backup"));
+                        CreateClientServersList(path);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Servers list doesn't exist and new one will be created.");
+                        CreateClientServersList(path);
+                    }
+                }
+                else if (c.Equals("N", StringComparison.OrdinalIgnoreCase))
+                {
+                    //Wrong path, ask for path
+                    Console.Write("Please provide the correct path to NoIPChat Client folder. ");
+                    string? path1 = Console.ReadLine();
+                    if (path1 != null)
+                    {
+                        path = path1;
+                        goto checkpath;
+                    }
+                    else
+                    {
+                        Console.Write("Type B to go back to Client menue.");
+                        goto input;
+                    }
+                }
+                else if (c.Equals("B", StringComparison.OrdinalIgnoreCase))
+                {
+                    Client();
+                }
+                else
+                {
+                    Console.WriteLine("Unknown. Try again.");
+                    goto clientconfigstart;
+                }
+            }
+        }
+        static void CreateClientServersList(string path)
+        {
+            Console.Write("How many known servers do you have?");
+            List<Client.Servers> servers = [];
+            if (Int32.TryParse(Console.ReadLine(), out int n))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine($"Enter data for server {i}.");
+                    Console.Write("Server name: ");
+                    string? name = Console.ReadLine();
+                    Console.Write("IP address: ");
+                    string? ip = Console.ReadLine();
+                    Console.Write("Port: ");
+                    if (Int32.TryParse(Console.ReadLine(), out int port))
+                    {
+                        if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(ip))
+                        {
+                            servers.Add(new Client.Servers() { Name=name, IP=ip, Port=port });
+                            Console.WriteLine($"Server {i} added successfully.");
+                        }
+                    }
+                }
+                File.WriteAllText(path,JsonSerializer.Serialize(servers));
             }
         }
         static void Begin()
