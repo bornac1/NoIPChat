3631470b963cb099f9ecc8790591894cb3a9eb0e
bornac1 <borna.cesarec1@gmail.com>
2024-12-18 08:00:48 +0100

    Restructure file format

--- ConfigurationData/Configuration.cs
+++ ConfigurationData/Configuration.cs
@@ -36,3 +36,24 @@ namespace ConfigurationData
         public string? Logfile { get; set; }
     }
 }
+namespace Client
+{
+    /// <summary>
+    /// Servers object.
+    /// </summary>
+    public struct Servers
+    {
+        /// <summary>
+        /// name of the server.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Server IP.
+        /// </summary>
+        public string IP { get; set; }
+        /// <summary>
+        /// Server port.
+        /// </summary>
+        public int Port { get; set; }
+    }
+}
